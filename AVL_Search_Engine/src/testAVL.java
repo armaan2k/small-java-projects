aeac2cf9f48210617893f39b9d69f567d6e800a7
armaan2k <43624419+armaan2k@users.noreply.github.com>
2019-12-30 16:38:05 -0500

    updating avl test

--- AVL_Search_Engine/src/testAVL.java
+++ AVL_Search_Engine/src/testAVL.java
@@ -2,6 +2,8 @@
 Name: Armaan Merchant
 Description: test for AVL tree implementation
  */
+import java.sql.SQLOutput;
+import java.util.Scanner;
 public class testAVL {
 
   public static void main(String[] args) {
@@ -14,15 +16,26 @@ public class testAVL {
     tree.put(1, "carl");
     tree.put(15, "fish");
 
-    System.out.println(tree.searchPath(15));
-    System.out.println(tree.get(10));
-    System.out.println(tree.get(15));
-    System.out.println(tree.get(111));
+    System.out.println("Testing fuctionality of AVL tree:");
+    System.out.println("Search path for node 15: " + tree.searchPath(15));
+    System.out.println("Searching for node 10: " + tree.get(10));
+    System.out.println("Searching for node 15: " + tree.get(15));
+    System.out.println("Searching for node 1: " + tree.get(1));
 
 
-    WebPageIndex webIndex = new WebPageIndex("doc1-arraylist.txt");
+    WebPageIndex webIndex = new WebPageIndex("search_file.txt");
     AVLtree webIndexAVL = webIndex.intoAVL();
-    System.out.println(webIndexAVL.get("array"));
+
+    Scanner input = new Scanner(System.in);
+    while(true){
+      System.out.println("\nEnter your search word or 'exit' to Quit:");
+      String search_word = input.nextLine();
+      if(search_word.equals("exit")){
+        break;
+      }
+      System.out.println(search_word + " has come up " + webIndexAVL.get(search_word) + " times");
+
+    }
 
 
   }
